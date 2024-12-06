9c6e1e2da99ed9657fd0ff1395390a36b92068cf
Daniil Katkov <klinovoe9204@gmail.com>
2024-12-06 16:41:50 +0200

    Change homework 5

--- homework/daniil_katkov/homework_5/task_2.py
+++ homework/daniil_katkov/homework_5/task_2.py
@@ -2,17 +2,17 @@ result_1 = 'результат операции: 42'
 result_2 = 'результат операции: 514'
 result_3 = 'результат работы программы: 9'
 
-result_for_1 = result_1.index('42')
+result_for_1 = result_1.index(':') + 2
 int_result_1 = int(result_1[result_for_1:]) + 10
 
 print(int_result_1)
 
-result_for_2 = result_2.index('514')
+result_for_2 = result_2.index(':') + 2
 int_result_2 = int(result_2[result_for_2:]) + 10
 
 print(int_result_2)
 
-result_for_3 = result_3.index('9')
+result_for_3 = result_3.index(':') + 2
 int_result_3 = int(result_3[result_for_3:]) + 10
 
 print(int_result_3)
