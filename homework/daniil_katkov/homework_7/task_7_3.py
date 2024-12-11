41d6dc6bf5d628b724844ba1c8a4560d2131063d
Daniil Katkov <klinovoe9204@gmail.com>
2024-12-11 16:20:58 +0200

    Lint

--- homework/daniil_katkov/homework_7/task_7_3.py
+++ homework/daniil_katkov/homework_7/task_7_3.py
@@ -3,18 +3,22 @@ str_2 = 'результат операции: 54'
 str_3 = 'результат работы программы: 209'
 str_4 = 'результат: 2'
 
+
 def sum(str_0):
     str_number = str_0.index(':') + 2
     str_sum = int(str_0[str_number:]) + 10
     print(str_sum)
 
+
 sum(str_4)
 
 ############################################################
+
 
 def sum(str_0):
     str_number = str_0.split()
     last_element = int(str_number[-1]) + 10
     print(last_element)
+
 
 sum(str_3)
