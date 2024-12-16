7fe38291b6d5f154e45ee6d8e1a958321195b5e8
Daniil Katkov <klinovoe9204@gmail.com>
2024-12-16 18:52:10 +0200

    Второе задание никак не получается сделать. Если генератор вроде получилось правильно написать, то с отображением нужных элементов есть проблема. Нужна помощь

---
+++ homework/daniil_katkov/homework_8/task_8_2.py
@@ -0,0 +1,17 @@
+def gen():
+    start = 0
+    start_1 = 1
+    while True:
+        yield start
+        start, start_1 = (start_1, start + start_1)
+
+fib_numbers = []
+
+
+count = 1
+for element in gen():
+    if count == 1000:
+        fib_numbers.append(element)
+        print(fib_numbers)
+        break
+    count +=1
