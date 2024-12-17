b2b2e1e0d0392a7f791efcceb9c7e6f9a6c2c43b
Daniil Katkov <klinovoe9204@gmail.com>
2024-12-17 10:43:39 +0200

    Вроде бы получилось. Можно и через список выводить результат. И по каждому значению отдельно

--- homework/daniil_katkov/homework_8/task_8_2.py
+++ homework/daniil_katkov/homework_8/task_8_2.py
@@ -8,11 +8,34 @@ def gen():
 
 fib_numbers = []
 
+selected_values = [5, 200, 1000, 100000]
 
 count = 1
 for element in gen():
-    if count == 1000:
+    if count in selected_values:
         fib_numbers.append(element)
-        print(fib_numbers)
+    if count == selected_values[-1]:
+        break
+    count += 1
+
+print(fib_numbers)
+
+####################################################################
+
+def gen():
+    start = 0
+    start_1 = 1
+    while True:
+        yield start
+        start, start_1 = (start_1, start + start_1)
+
+
+selected_values = [5, 200, 1000, 100000]
+
+count = 1
+for element in gen():
+    if count in selected_values:
+        print(element)
+    if count == selected_values[-1]:
         break
     count += 1
