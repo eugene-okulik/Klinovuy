d88b5a32e34ed546862c2e094e9c57a4256b93b6
Daniil Katkov <klinovoe9204@gmail.com>
2024-12-18 16:20:28 +0200

    Lint

--- homework/daniil_katkov/homework_9/task_9_2.py
+++ homework/daniil_katkov/homework_9/task_9_2.py
@@ -1,7 +1,7 @@
 temperatures = [
-                20, 15, 32, 34, 21, 19, 25, 27, 30, 32, 34, 30, 29, 25, 27, 22, 22, 23, 25, 29, 29, 31, 33, 31,
-                30, 32, 30, 28, 24, 23
-               ]
+        20, 15, 32, 34, 21, 19, 25, 27, 30, 32, 34, 30, 29, 25, 27, 22, 22, 23, 25, 29, 29, 31, 33, 31,
+        30, 32, 30, 28, 24, 23
+        ]
 
 hot_temperatures = list((filter(lambda x: x > 28, temperatures)))
 
@@ -11,13 +11,13 @@ print(sum(hot_temperatures) / len(hot_temperatures))
 
 ############################################################################
 
+
 def new_elements(element):
     if element > 28:
         return element
     return None
 
+
 hot_temperatures = list(map(new_elements, temperatures))
 
 print(hot_temperatures)
-
-
