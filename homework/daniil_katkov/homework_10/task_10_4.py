07b6842c9bee165b818a057035f6a4a8d6367716
Daniil Katkov <klinovoe9204@gmail.com>
2025-01-14 19:11:17 +0200

    С Новым Годом!

---
+++ homework/daniil_katkov/homework_10/task_10_4.py
@@ -0,0 +1,12 @@
+PRICE_LIST = '''тетрадь 50р
+книга 200р
+ручка 100р
+карандаш 70р
+альбом 120р
+пенал 300р
+рюкзак 500р'''
+
+new_dict =  {name: int(price[:-1]) for name, price in (element.split(' ', 1)
+             for element in PRICE_LIST.splitlines())
+            }
+print(new_dict)
