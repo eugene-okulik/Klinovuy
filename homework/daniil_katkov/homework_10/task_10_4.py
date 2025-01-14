bafbde59e8830a9cfbd77b1765a276c5852f6f6f
Daniil Katkov <klinovoe9204@gmail.com>
2025-01-14 19:57:38 +0200

    Update

--- homework/daniil_katkov/homework_10/task_10_4.py
+++ homework/daniil_katkov/homework_10/task_10_4.py
@@ -9,4 +9,5 @@ PRICE_LIST = '''тетрадь 50р
 new_dict =  {name: int(price[:-1]) for name, price in (element.split(' ', 1)
              for element in PRICE_LIST.splitlines())
             }
+
 print(new_dict)
