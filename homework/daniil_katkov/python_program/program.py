4084d5b48db9572c5c1a22621d6199f9f0c0ecb8
Daniil Katkov <klinovoe9204@gmail.com>
2025-03-01 14:46:55 +0200

    Update

--- homework/daniil_katkov/python_program/program.py
+++ homework/daniil_katkov/python_program/program.py
@@ -20,4 +20,12 @@ args = parser.parse_args()
 all_files = glob.glob(f"{args.folder}/*")
 
 for line_number, line_text in search_log_file(all_files, args.text):
-    print(f"Найдено в строке {line_number}: {line_text}")
+    words = line_text.split()
+    found_index = words.index(args.text)
+    start_index = max(0, found_index - 5)
+    end_index = min(len(words), found_index + 6)
+    words_before = words[start_index:found_index]
+    words_after = words[found_index:end_index]
+    new_str1 = ("..." if start_index > 0 else "") + ' '.join(words_before)
+    new_str2 = ' '.join(words_after) + ("..." if end_index < len(words) else "")
+    print(f"Найдено в строке {line_number}: {new_str1} {new_str2}")
